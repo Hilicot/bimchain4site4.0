d67c095e8fc803be982ed947cdcd1072a5dc3b0f
Hilicot <Hilicot@users.noreply.github.com>
2023-02-22 14:49:03 +0000

    deploy: 332a884ce1c9f24c95bbb264424406e1c6ac4f35

---
+++ static/js/5.9372a8ad.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbimchain4site4.0"]=this["webpackJsonpbimchain4site4.0"]||[]).push([[5],{469:function(c,e,i){"use strict";i.r(e);i(0);var t=i(33),s=i(76),n=i(9);e.default=function(){return Object(n.jsx)(s.p,{children:Object(n.jsx)(s.b,{children:Object(n.jsx)(s.k,{children:Object(n.jsx)(t.b,{})})})})}}}]);
+//# sourceMappingURL=5.9372a8ad.chunk.js.map
---
+++ static/js/6.ec84ef6b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbimchain4site4.0"]=this["webpackJsonpbimchain4site4.0"]||[]).push([[6],{470:function(t,c,e){"use strict";e.r(c);var i=e(0),n=e(61),a=e(33),s=e(183),u=e(9);c.default=function(){var t=Object(n.a)();return Object(i.useEffect)((function(){t(Object(s.c)())}),[t]),Object(u.jsx)(a.a,{to:"/auth/login",replace:!0})}}}]);
+//# sourceMappingURL=6.ec84ef6b.chunk.js.map
